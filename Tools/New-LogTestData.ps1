ed05ccc028603ed5a3b30a6f39d00000d1c2f265
Marek Sledzinski <sledzinski.marek@gmail.com>
2016-12-06 22:36:36 +0100

    reworks - data initialization

--- Tools/New-LogTestData.ps1
+++ Tools/New-LogTestData.ps1
@@ -1,6 +1,5 @@
-﻿$eventLogName = 'PSWorkshopService'
-
-$appName = 'PSWorkshopServiceApp'
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$applications = (Get-Content -Path (Join-Path $here '..\Data\applications.json')) | ConvertFrom-Json | % applications
 
 $healthCheckSource = 'HealthCheckSystem'
 
@@ -9,22 +8,6 @@ if ([System.Diagnostics.EventLog]::SourceExists($healthCheckSource) -eq $false)
     [System.Diagnostics.EventLog]::CreateEventSource($healthCheckSource, "Application")
 }
 
-# create event log
-function Test-WorkshopLog
-{
-    ((Get-WinEvent -ListLog * | ? LogName -eq $eventLogName | Measure-Object | % Count) -eq 1)        
-}
-
-if(-not (Test-WorkshopLog))
-{
-    New-EventLog -LogName PSWorkshopService -Source scripts
-
-    Limit-EventLog -LogName PSWorkshopService -OverflowAction OverwriteAsNeeded -MaximumSize 1MB
-
-    Get-WinEvent -ListLog * | ? LogName -eq PSWorkshopService
-}
-
-# produce some events - custom log
 function Write-ToEvents
 {
     param
@@ -38,16 +21,43 @@ function Write-ToEvents
     Write-EventLog -LogName $LogName -Source $Source -Message $Message -EventId 3005 -EntryType $EntryType
 }
 
-1..10 | Foreach { Write-ToEvents -LogName $eventLogName -Source 'Scripts' -Message "Error from service $appName" -EntryType Error }
-1..5 | Foreach { Write-ToEvents -LogName $eventLogName -Source 'Scripts' -Message "Some information about $appName" -EntryType Information }
-
-1..3 | Foreach { Write-ToEvents -LogName 'Application' -Source $healthCheckSource -Message "Uncaught exception from $appName. check service health." -EntryType Error }
-
-# setup information about application
-$registryPath = "HKLM:\SOFTWARE\$appName"
-if (-not (Test-Path $registryPath))
+function Test-WorkshopLog
 {
-    New-Item $registryPath
-    New-ItemProperty -Path $registryPath -Name 'AppLogName' -Value $eventLogName -PropertyType String -Force | Out-Null
+    param([string]$eventLogName)
+
+    ((Get-WinEvent -ListLog * | ? LogName -eq $eventLogName | Measure-Object | % Count) -gt 0)        
+}
+
+foreach($appItem in $applications)
+{
+    $eventLogName = $appItem.name
+    $appName =  $appItem.name
+    $registryKey = $appItem.registryKey
+    $source = "scripts$($appItem.otherStting)"
+
+    if(-not (Test-WorkshopLog $eventLogName))
+    {
+        New-EventLog -LogName $eventLogName -Source $source
+
+        Limit-EventLog -LogName $eventLogName -OverflowAction OverwriteAsNeeded -MaximumSize 1MB
+
+        Get-WinEvent -ListLog * | ? LogName -eq $eventLogName
+    }
+
+    # produce some events - custom log
+
+
+    1..10 | Foreach { Write-ToEvents -LogName $eventLogName -Source $source -Message "Error from service $appName" -EntryType Error }
+    1..5 | Foreach { Write-ToEvents -LogName $eventLogName -Source $source -Message "Some information about $appName" -EntryType Information }
+
+    1..3 | Foreach { Write-ToEvents -LogName 'Application' -Source $healthCheckSource -Message "Uncaught exception from $appName. check service health." -EntryType Error }
+
+    # setup information about application
+    $registryPath = "HKLM:\SOFTWARE\$registryKey"
+    if (-not (Test-Path $registryPath))
+    {
+        New-Item $registryPath
+        New-ItemProperty -Path $registryPath -Name 'AppLogName' -Value $eventLogName -PropertyType String -Force | Out-Null
+    }
 }
 
